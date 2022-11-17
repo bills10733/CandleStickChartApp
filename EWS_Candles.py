38e9e8171667ee64cf5061412cc39f08c5058b17
bills10733 <118470437+bills10733@users.noreply.github.com>
2022-11-17 13:20:09 -0400

    Initial commit

---
+++ EWS_Candles.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import streamlit as st
+from cnbcfinance.cnbc import Cnbc
+import plotly.graph_objects as go
+#from datetime import datetime
+import datetime
+
+st.sidebar.write('sidebar')
+
+st.header('EWS Daily Signal - SPX')
+
+
+cnbc = Cnbc('SPX')
+df = cnbc.get_history_df(interval='1d')
+
+df.reset_index(inplace=True)
+
+df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+df.set_index('datetime')
+
+print(df)
+
+#quote = cnbc.get_quote()
+#quote[0]['last']
+
+fig = go.Figure(data=[go.Candlestick(x=df['datetime'],
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'])])
+fig.update_xaxes(type='category')
+st.plotly_chart(fig, use_container_width=True)
+#fig.show()
